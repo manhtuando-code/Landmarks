f6dcda1f76d28bce0f90df8df96a22e81c9d1c5d
Tuan <tuando22092000@gmail.com>
2022-05-15 14:51:50 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Do Tuan on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
